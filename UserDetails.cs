3be43d746b07c185b14cfc59b1222f7d15b9c49e
fairydhari <46656807+fairydhari@users.noreply.github.com>
2019-01-23 07:10:52 +0530

    Add files via upload

---
+++ UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace webapieg.Models
+{
+    public class UserDetails
+    {
+        public int userId{get;set;}
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string mobilephone { get; set; }
+    }
+}
