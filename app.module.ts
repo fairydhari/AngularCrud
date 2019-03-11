91598eff375711a018e2e62af9aefebfdfd3a6f7
fairydhari <46656807+fairydhari@users.noreply.github.com>
2019-03-11 07:16:47 +0530

    module

--- app.module.ts
+++ app.module.ts
@@ -1,39 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
-import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
-import { AppRoutingModule } from './app-routing.module' //<-- import
-import { EmployeeService } from './services/employee.service';
+
 import { GridModule } from '@progress/kendo-angular-grid';
-import { SliderModule } from '@progress/kendo-angular-inputs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { DialogModule } from '@progress/kendo-angular-dialog';
-import { ModalModule } from 'angular-custom-modal';
-import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { GridEgComponent } from './grid-eg/grid-eg.component';
+import { DatePipe } from '@angular/common';
+import { StatusFilterComponent } from './status-filter/status-filter.component'
+import { DropDownListModule } from '@progress/kendo-angular-dropdowns';
 
 
 @NgModule({
   declarations: [
-    AppComponent,    
-    EmployeeListComponent,
-    CreateEmployeeComponent,
-   ],
+    AppComponent,
+    GridEgComponent,
+    StatusFilterComponent
+  ],
   imports: [
     BrowserModule,
-    HttpClientModule,    
-    AppRoutingModule,
-    BrowserAnimationsModule, GridModule, SliderModule,
-    DialogModule,
-    ModalModule,
-    ReactiveFormsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    GridModule,
+    DropDownListModule
   ],
-  providers: [
-    EmployeeService
-    ],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
